e860e06726c8a4b72e2ae3e307274610d9009e11
Andreas <jakobsson.adde@gmail.com>
2017-07-03 15:15:45 +0200

    schema

--- src/models/timeTable.js
+++ src/models/timeTable.js
@@ -1,11 +1,10 @@
 var mongoose = require('mongoose');
 
 var timeTableSchema = new mongoose.Schema({
-  date: new ISODate() {
-    startTime: String,
-    endTime: String,
-    break: Number
-  }
+  date: { type: Date, default: Date.now},
+  start: { type: Number, default: 0 },
+  end: { type: Number, default: 0 },
+  break: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('TimeTable', timeTableSchema);
